df8bd2d843a69a53096ddf2533bfc3e6380ec94b
Polina <polinaantonovna@yandex.com>
2023-12-04 01:30:31 +0700

    Added comments for locators

--- Tests/locators.py
+++ Tests/locators.py
@@ -1,30 +1,30 @@
 from selenium.webdriver.common.by import By
 class Locators:
     #section below with variables for login page:
-    page_login_url = 'https://stellarburgers.nomoreparties.site/login'
-    field_email_xpath = ".//input[(@class = 'text input__textfield text_type_main-default' and @type='text')]"
-    field_password_xpath = ".//input[(@class = 'text input__textfield text_type_main-default' and @type='password')]"
-    page_login_button_xpath = ".//button[(@class = 'button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa')]"
-    page_login_registration_link = ".//a[(@class = 'Auth_link__1fOlj')]"
+    page_login_url = 'https://stellarburgers.nomoreparties.site/login' #url страницы логин
+    field_email_xpath = ".//input[(@class = 'text input__textfield text_type_main-default' and @type='text')]" #email поле
+    field_password_xpath = ".//input[(@class = 'text input__textfield text_type_main-default' and @type='password')]" #password поле
+    page_login_button_xpath = ".//button[(@class = 'button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa')]" #кнопка войти
+    page_login_registration_link = ".//a[(@class = 'Auth_link__1fOlj')]" #ссылка Зарегестрироваться
     #main page:
-    page_main_url = "https://stellarburgers.nomoreparties.site/"
-    page_main_name_label_xpath = ".//h1[(@class = 'text text_type_main-large mb-5 mt-10')]"
-    page_main_personal_account_tab_xpath = ".//p[@class='AppHeader_header__linkText__3q_va ml-2' and text()='Личный Кабинет']"
-    page_main_constructor_tab_xpath = ".//p[@class='AppHeader_header__linkText__3q_va ml-2' and text()='Конструктор']"
-    page_main_constructor_logo_xpath = ".//div[@class='AppHeader_header__logo__2D0X2']"
+    page_main_url = "https://stellarburgers.nomoreparties.site/" #url главной страницы Конструктор
+    page_main_name_label_xpath = ".//h1[(@class = 'text text_type_main-large mb-5 mt-10')]" #заголовок страницы
+    page_main_personal_account_tab_xpath = ".//p[@class='AppHeader_header__linkText__3q_va ml-2' and text()='Личный Кабинет']" #вкладка Личный кабинет
+    page_main_constructor_tab_xpath = ".//p[@class='AppHeader_header__linkText__3q_va ml-2' and text()='Конструктор']" #вкладка Конструктор
+    page_main_constructor_logo_xpath = ".//div[@class='AppHeader_header__logo__2D0X2']" #лого
     page_main_button_gradient_button_xpath = ".//button[(@class = 'button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_large__G21Vg')]" #кнопка - оформить заказ
-    page_main_sections_xpath = ".//span[(@class='text text_type_main-default')][2]"
+    page_main_sections_xpath = ".//span[(@class='text text_type_main-default')][2]" # секции - доработать
     #registration page:
-    page_registration_url="https://stellarburgers.nomoreparties.site/register"
-    page_registration_register_button = (".//button[(@class='button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa')]")
+    page_registration_url="https://stellarburgers.nomoreparties.site/register" #url регистрации
+    page_registration_register_button = (".//button[(@class='button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa')]") #кнопка Зарегистрироваться
     page_registration_enter_button = ".//a[(@class = 'Auth_link__1fOlj' and text()='Войти')]" #кнопка - войти
-    page_registration_name_field = (By.CSS_SELECTOR, "input[name='name']")
-    page_registration_email_field = (By.CSS_SELECTOR, "input[name='email']")
-    page_registration_password_field = (By.CSS_SELECTOR, "input[type='password']")
+    page_registration_name_field = By.XPATH, './/fieldset[1]//input' #имя
+    page_registration_email_field = By.XPATH, './/fieldset[2]//input' #email
+    page_registration_password_field = By.XPATH, './/fieldset[3]//input' #password
     #forgot password page:
-    page_forgot_password_url = "https://stellarburgers.nomoreparties.site/forgot-password"
+    page_forgot_password_url = "https://stellarburgers.nomoreparties.site/forgot-password" #url восстановление пароля
     page_forgot_password_enter_button = ".//a[(@class = 'Auth_link__1fOlj' and text()='Войти')]" #кнопка - войти
     #personal account page:
-    page_personal_account = "https://stellarburgers.nomoreparties.site/account/profile"
-    page_personal_account_logout_button_xpath = ".//button[(@class = 'Account_button__14Yp3 text text_type_main-medium text_color_inactive' and text()='Выход' )]"
-    page_personal_account_label_here_you_can = (By.XPATH,".//p[@class='Account_text__fZAIn text text_type_main-default' and text()='В этом разделе вы можете изменить свои персональные данные']")
+    page_personal_account = "https://stellarburgers.nomoreparties.site/account/profile" # url личный кабинет
+    page_personal_account_logout_button_xpath = ".//button[(@class = 'Account_button__14Yp3 text text_type_main-medium text_color_inactive' and text()='Выход' )]" #кнопка Выход
+    page_personal_account_label_here_you_can = (By.XPATH,".//p[@class='Account_text__fZAIn text text_type_main-default' and text()='В этом разделе вы можете изменить свои персональные данные']") # текст В этом разделе вы можете
--- Tests/test_registration_page.py
+++ Tests/test_registration_page.py
@@ -17,9 +17,9 @@ class TestRegistration:
         WebDriverWait(driver, 3) \
             .until(expected_conditions
                    .visibility_of_element_located((By.XPATH, Locators.page_registration_enter_button)))
-       # driver.find_element(Locators.page_registration_name_field).send_keys("Fyz")
-       # driver.find_element(Locators.page_registration_email_field).send_keys(fake.email())
-       # driver.find_element(Locators.page_registration_email_field).send_keys(fake.password())
+        driver.find_element(Locators.page_registration_name_field).send_keys("Fyz")
+        driver.find_element(Locators.page_registration_email_field).send_keys(fake.email())
+        driver.find_element(Locators.page_registration_email_field).send_keys("123456")
         assert driver.current_url == Locators.page_registration_url
 
 
